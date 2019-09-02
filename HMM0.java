4057c4a88ffdd3a50fa793efd0a4d50bef75ae1f
James Tan <jamestancheemin98@gmail.com>
2019-09-02 20:19:30 +0200

    changed name from main to HMM0

---
+++ HMM0.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+public class HMM0{
+    public static void main(String[] args){
+       
+        Matrix transition_matrix = Matrix.create_matrix();
+        // System.out.println("transition done");
+        // System.out.println(transition_matrix);
+        Matrix emission_matrix = Matrix.create_matrix();
+        // System.out.println(emission_matrix);
+        // System.out.println("emission done");
+
+        Matrix initial_state_matrix = Matrix.create_matrix();
+        // System.out.println(initial_state_matrix);
+        // System.out.println("initial done");
+
+        // System.out.println("Starting multiplication...");
+        Matrix next_state = initial_state_matrix.multiply(transition_matrix);
+        Matrix results = next_state.multiply(emission_matrix);
+        // System.out.println("Multiplication done.. Printing results now");
+        System.out.println(results);
+
+    }
+}
