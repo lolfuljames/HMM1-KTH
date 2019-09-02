eff81c537444cdca23ac748e9aeac93870bc4b30
James Tan <jamestancheemin98@gmail.com>
2019-09-02 15:56:16 +0200

    completed create_matrix and toString for matrix

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+
 public class Matrix{
+    
     private double[][] elements;
     private int row;
     private int col;
@@ -7,20 +12,51 @@ public class Matrix{
     public void set_col(int col){this.col = col;}
     public void set_elements(double[][] elements){this.elements = elements;}
 
-    public void get_row(){return this.row;}
-    public void get_col(){return this.col;}
-    public void get_elements(){return this.element;}
+    public int get_row(){return this.row;}
+    public int get_col(){return this.col;}
+    public double[][] get_elements(){return this.element;}
+
+    public Matrix(){};
+
+    public Matrix(int row, int col, double[][] elements){
+        this.row = row;
+        this.col = col;
+        this.elements = elements;
+    }
 
     public String toString(){
         String elements_string;
 
-        for(int j=0; j++; j<this.row){
-            for(int k=0; k++; k<this.col){
-                if(j==0 && k==0) elements_string = elements[j][k];
-                
+        for(int j=0; j<this.row; j++){
+            for(int k=0; k<this.col; k++){
+                if(j!=0 || k!= 0) elements_string = elements_string +  " " + Double.toString(this.elements[j][k]);
+
+                //initial string format [row] [col] [elements row by row]
+                else elements_string = Int.toString(row) + " " + Int.toString(col) + " " + Double.toString(this.elements[j][k]);
             }
         }
+
+        return elements_string;
     }
-    public static double[][] create_matrix()
-    public Matrix(){};
+
+    public static Matrix create_matrix(){
+        int row;
+        int col;
+        Scanner scan = new Scanner(System.in);
+
+        row = scan.nextInt();
+        col = scan.nextInt();
+
+        double[][] elements = new double[row][col];
+
+        for(int j=0; jrow; j++){
+            for(int k=0; k<col; k++){
+                elements[j][k] = scan.nextDouble();
+            }
+        }
+        scan.next();
+        
+        return Matrix(row,col,elements);
+    }
+    
 }
