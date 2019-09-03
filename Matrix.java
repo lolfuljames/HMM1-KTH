099cf73794f58e21ae2a125acc499789758b9a1a
James Tan <jamestancheemin98@gmail.com>
2019-09-03 16:51:17 +0200

    added max and argmax for multiplication

--- Matrix.java
+++ Matrix.java
@@ -52,7 +52,11 @@ public class Matrix{
 
         //Sanity check for matrix dimensions
         if(this.col != B.row) {
-            System.out.printf("Error! Incorrect dimensions for matrix multiplication for matrix A dimension %dx%d and matrix B dimension %dx%d", this.row, this.col, B.row, B.col);
+            System.out.printf("Error! Incorrect dimensions for matrix multiplication for matrix A dimension %dx%d and matrix B dimension %dx%d\n", this.row, this.col, B.row, B.col);
+            System.out.print("Matrix A: ");
+            System.out.println(this.toString());
+            System.out.print("Matrix B: ");
+            System.out.println(B);
             return new Matrix();
         }
 
@@ -75,6 +79,53 @@ public class Matrix{
             }
         }
 
+        return new Matrix(result_row,result_col,result_elements);
+    }
+
+    public Matrix multiply_max(Matrix B, ArrayList<Matrix> prev_state){
+        int result_row = this.row;
+        int result_col = B.col;
+
+        //Sanity check for matrix dimensions
+        if(this.col != B.row) {
+            System.out.printf("Error! Incorrect dimensions for matrix multiplication for matrix A dimension %dx%d and matrix B dimension %dx%d\n", this.row, this.col, B.row, B.col);
+            System.out.print("Matrix A: ");
+            System.out.println(this.toString());
+            System.out.print("Matrix B: ");
+            System.out.println(B);
+            return new Matrix();
+        }
+
+        //Copying over elements data
+        double[][] A_elements = this.get_elements();
+        double[][] result_elements = new double[result_row][result_col];
+        double[][] B_elements = B.get_elements();
+
+        //Calling over intermediate array to store most possible previous state indices
+        double[][] state_index = new double[result_row][result_col];
+
+        //Actual multiplication
+        for(int i=0; i<result_row; i++){
+            for(int j=0; j<result_col; j++){
+                for(int k=0; k<this.col; k++){
+
+                    //Using natural log (ln) to perform multiplication
+                    //Log(x*y) = Log(x) + Log(y)
+                    double result = Math.log(A_elements[i][k])+Math.log(B_elements[k][j]); 
+                    result = Math.exp(result);
+                    //first time adding entry to state array
+                    if(k==0){
+                        result_elements[i][j] = result;
+                        state_index[i][j] = k;
+                    }
+                    else if(result_elements[i][j] < result) {
+                        result_elements[i][j] = result;
+                        state_index[i][j] = k;
+                    }
+                }
+            }
+        }
+        prev_state.add(new Matrix(result_row, result_col, state_index));
         return new Matrix(result_row,result_col,result_elements);
     }
     
