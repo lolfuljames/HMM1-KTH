35a02683651b11e2f353b23ab77ba0e6cb9420b2
James Tan <jamestancheemin98@gmail.com>
2019-09-03 14:17:19 +0200

    fixed HMM1 by changing alpha1 calculations

--- HMM1.java
+++ HMM1.java
@@ -1,25 +1,45 @@
 import java.util.*;
 import java.io.*;
+
 public class HMM1{
     
     private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
        
+        /*
+         *  Matrix where row = current state, col = next state (timestep), elements = joint probability of state entering next state (row -> col)
+         */
         Matrix transition_matrix = create_matrix();
         // System.out.println("transition done");
         // System.out.println(transition_matrix);
+
+        
+        /*
+         *  Matrix where row = current state, col = observation, elements = joint probability of state and observation
+         */
         Matrix emission_matrix = create_matrix();
         // System.out.println(emission_matrix);
         // System.out.println("emission done");
 
+
+        /*
+         *  Matrix where row size = 1, col = states, elements = probability of entering initial state (col)
+         */
         Matrix initial_state_matrix = create_matrix();
         // System.out.println(initial_state_matrix);
         // System.out.println("initial done");
 
+        /*
+         *  Array of observations where array[0] is the first observation, array[n-1] is the n-th observation
+         */
         int[] observation_sequence = create_observation_sequence(); 
         // System.out.println(observation_sequence);
         // System.out.println("observation done");
 
+
+        /*
+         *  Matrix where row = alpha at time(row), col = states, elements = probability of entering state (col) given sequence of previous observations
+         */
         ArrayList<Matrix> alpha_matrix = new ArrayList<Matrix>();
 
         alpha_pass(alpha_matrix, observation_sequence, emission_matrix, transition_matrix, initial_state_matrix, observation_sequence.length);
@@ -29,6 +49,14 @@ public class HMM1{
         // System.out.println("Multiplication done.. Printing results now");
         // System.out.println(results);
 
+        Matrix final_alpha = alpha_matrix.get(alpha_matrix.size()-1);
+        double total = 0;
+
+        for(int i=0; i<final_alpha.get_elements()[0].length; i++){
+            total += final_alpha.get_elements()[0][i];
+        }
+
+        System.out.println(total);
     }
 
     public static Matrix create_matrix(){
@@ -71,12 +99,24 @@ public class HMM1{
      * Function used to initialise forward algorithm
      */
     public static void alpha_pass(ArrayList<Matrix> alpha_matrix, int[] observation_sequence, Matrix emission_matrix, Matrix transition_matrix, Matrix initial_state_matrix, int timestep){
-        if(alpha_matrix.size() == 0){
-           Matrix state1 = initial_state_matrix.multiply(transition_matrix);
-           System.out.println(state1);
-           alpha_matrix.add(alpha_multiply_observation(state1, emission_matrix, observation_sequence[0]));
-        } while(alpha_matrix.size() != timestep){
-            break;
+        
+        Matrix next_state = new Matrix();
+        Matrix current_state = new Matrix();
+        
+        while(alpha_matrix.size() != timestep){
+
+            //calculate alpha(1)
+            // alpha(1) = B*pi (written below) *P(x|0)
+            if(alpha_matrix.size() == 0) next_state = initial_state_matrix;//.multiply(transition_matrix);
+            //UNSURE!!!  //TODO calculate's alpha(2...T)
+            else next_state = current_state.multiply(transition_matrix);
+
+            next_state = alpha_multiply_observation(next_state, emission_matrix, observation_sequence[alpha_matrix.size()]);
+            alpha_matrix.add(next_state);
+            // System.out.println(next_state);
+
+            // alpha is passed down and saved
+            current_state = next_state;
         }
     }
 
